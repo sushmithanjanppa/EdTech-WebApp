6df8523b05238663609ca2e84ea220ddc42892e7
sushmithanjanppa <92929680+sushmithanjanppa@users.noreply.github.com>
2022-05-05 23:21:47 -0400

    Updated filter routes
    Updated filter routes

--- routes/filter.js
+++ routes/filter.js
@@ -3,9 +3,17 @@ const router = express.Router();
 const data = require('../data');
 const courseData = data.courses;
 
+
+
+router.get('/', async (req, res) => {
+    res.render('edu/filterIndex', { title: `Select you Branch` });
+});
+
 router.get('/filter/:key', async (req, res) => {
 
+    // console.log(req.params)
     let result = await courseData.getfilterByBranch(req.params.key);
+    console.log(result)
     let resultArr = [];
     for (let i = 0; i < result.length ; i++) {
         resultArr.push(result[i]);
@@ -20,6 +28,23 @@ router.get('/filter/:key', async (req, res) => {
     }
     
     res.render('edu/filter', { title: 'Courses' , data: resultArr, filterSearchTerm: req.params.key, error: error, hasError: hasError });
+});
+
+
+router.get('/course/:id', async (req, res) => {
+    if (!req.params.id && req.params.id.trim() === '') {
+        return res.status(400).json({ status: 400, error: `Invalid id`, home: "http://localhost:3000" });
+    }
+    let result = await courseData.get(req.params.id);
+    let error = '';
+    let hasError = false;
+    if (!result) {
+        hasError = true;
+        error = `No courses found for the given id ${req.params.id}`
+        return res.status(404).json({ error: error });
+    }
+    
+    res.render('edu/courseContent', { title: result.name, data: result, error: error, hasError: hasError });
 });
 
 module.exports = router;
